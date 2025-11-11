f8b6695a4f749a21439a3187e19583cc52ae3b2d
karshhh <karshhhvidya@gmail.com>
2025-11-11 18:51:28 +0530

    Added last commit brhjbvbdhe

--- backend/src/main/java/com/pledge/backend/service/AuthService.java
+++ backend/src/main/java/com/pledge/backend/service/AuthService.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class AuthService {
     private static final String ADMIN_USERNAME = "Akash";
-    private static final String ADMIN_PASSWORD = "Akash@123";
+    private static final String ADMIN_PASSWORD = "Akash@123www";
 
     @Autowired
     private JwtUtil jwtUtil;
